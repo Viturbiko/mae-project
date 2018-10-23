7e5e05952f21f0cf39bd5b8e41011bba91f2d8e5
Viturbiko <vito.negrito@gmail.com>
2018-10-23 23:13:53 +0200

    Calendario empezando

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -6,6 +6,52 @@
 	<div class="content_calendar">
 
 		<h1>Events & Information</h1>
+
+		<hr align="left" width="5%">
+
+		<div class="calendar">
+			<div class="calendar_days">				
+				<div class="event_day">
+					<p>NOVEMBRE 2018</p>
+					<h1>29</h1>
+					<hr align="center" width="50%">
+					<p>Lisbon</p>
+					<p>Final Event</p>
+				</div>
+				<div class="event_day">
+					<p>NOVEMBRE 2018</p>
+					<h1>29</h1>
+					<hr align="center" width="50%">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.d</p>
+				</div>
+				<div class="event_day">
+					<p>NOVEMBRE 2018</p>
+					<h1>29</h1>
+					<hr align="center" width="50%">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.d</p>
+				</div>
+				<div class="event_day">
+					<p>NOVEMBRE 2018</p>
+					<h1>29</h1>
+					<hr align="center" width="50%">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.d</p>
+				</div>
+				<div class="event_day">
+					<p>NOVEMBRE 2018</p>
+					<h1>29</h1>
+					<hr align="center" width="50%">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.d</p>
+				</div>
+				<div class="event_day">
+					<p>NOVEMBRE 2018</p>
+					<h1>29</h1>
+					<hr align="center" width="50%">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.d</p>
+				</div>
+			</div>
+			<div class="calendar_events">
+			</div>
+		</div>
 		
 	</div>
 
