a1fc07f6d3d13ef5148439db28abb33778ee7109
Viturbiko <vito.negrito@gmail.com>
2018-10-30 22:02:24 +0100

    Events emepzando

--- resources/views/calendar_main.blade.php
+++ resources/views/calendar_main.blade.php
@@ -9,6 +9,9 @@
 
 <body>
 
+	<div class="bg_right_events"></div>
+	<div class="bg_left_events"></div>
+
 	@include('navbar')
 
 	<div class="calendar_header">
@@ -33,17 +36,132 @@
 
 		<div class="mae">
 
-		<p>MED ATLANTIC ECOBONUS</p>
+			<p>MED ATLANTIC ECOBONUS</p>
 
 		</div>
 		
-		<h1>CALENDARIO</h1>
+		<div class="events_container">
+			
+			<div class="event">
+				<img src="/images/events/event-1.jpg" alt="">
+				<div class="event_date">
+					<h1>28</h1>
+					<p>OCTOBER</p>
+				</div>
+				<div class="event_info">
+					<p><b>ESTE ES UN EJEMPLO DE EVENTO</b></p>
+					<p>SUBTITULO DEL EVENTO</p>
+				</div>
+				<img class="arrow_down" src="/images/events/flecha_down.png" alt="" onclick="showhide(1);">
+			</div>
+
+			<div class="extra_info" id="1">
+				<ul>
+					<li>To present and confront the external costs calculator (mainly assumptions, values, etc. used).</li>
+					<li>To assess the potential scientific/technical consensus towards an EU external cost calculator based on the MAE approach.</li>
+				</ul>				
+				<p>28 October 2018</p>
+				<p>Lisbon, Hélder Ribeiro Dias Cristóvão.</p>
+				<p>To register: mae.project@puertos.es</p>
+				<button class="btn">ADD TO CALENDAR</button>
+			</div>
+
+
+
+
+
+
+			<div class="event">
+				<img src="/images/events/event-1.jpg" alt="">
+				<div class="event_date">
+					<h1>28</h1>
+					<p>OCTOBER</p>
+				</div>
+				<div class="event_info">
+					<p><b>ESTE ES UN EJEMPLO DE EVENTO</b></p>
+					<p>SUBTITULO DEL EVENTO</p>
+				</div>
+				<img class="arrow_down" src="/images/events/flecha_down.png" alt="" onclick="showhide(2);">
+			</div>
+
+			<div class="extra_info" id="2">
+				<ul>
+					<li>To present and confront the external costs calculator (mainly assumptions, values, etc. used).</li>
+					<li>To assess the potential scientific/technical consensus towards an EU external cost calculator based on the MAE approach.</li>
+				</ul>				
+				<p>28 October 2018</p>
+				<p>Lisbon, Hélder Ribeiro Dias Cristóvão.</p>
+				<p>To register: mae.project@puertos.es</p>
+				<button class="btn">ADD TO CALENDAR</button>
+			</div>
+
+
+
+
+
+
+
+
+			<div class="event">
+				<img src="/images/events/event-1.jpg" alt="">
+				<div class="event_date">
+					<h1>28</h1>
+					<p>OCTOBER</p>
+				</div>
+				<div class="event_info">
+					<p><b>ESTE ES UN EJEMPLO DE EVENTO</b></p>
+					<p>SUBTITULO DEL EVENTO</p>
+				</div>
+				<img class="arrow_down" src="/images/events/flecha_down.png" alt="" onclick="showhide(3);">
+			</div>
+
+			<div class="extra_info" id="3">
+				<ul>
+					<li>To present and confront the external costs calculator (mainly assumptions, values, etc. used).</li>
+					<li>To assess the potential scientific/technical consensus towards an EU external cost calculator based on the MAE approach.</li>
+				</ul>				
+				<p>28 October 2018</p>
+				<p>Lisbon, Hélder Ribeiro Dias Cristóvão.</p>
+				<p>To register: mae.project@puertos.es</p>
+				<button class="btn">ADD TO CALENDAR</button>
+			</div>
+
+
+			
+
+
+		</div>
 
 	</div>
 
 	@include('contact')
 
 	@include('footer')
+
+	<script>		
+
+		function showhide(id){
+
+	        if (document.getElementById) {
+
+	          var div_id = document.getElementById(id);
+
+	          var divs = document.getElementsByClassName("extra_info");
+
+	            for(var i=0;i<divs.length;i++) {
+	              divs[i].style.display = "none";
+	              divs[i].previousElementSibling.style.backgroundColor = "white";
+	              divs[i].previousElementSibling.style.color = "black";
+	            }
+
+	          div_id.style.display = "block";
+	          div_id.previousElementSibling.style.backgroundColor = "#12162B";
+	          div_id.previousElementSibling.style.color = "white";
+	        } 
+	        return false;
+		 }
+
+	</script>
 
 </body>
 
