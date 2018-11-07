06ac303bbfbd73c498a520d85e62c11b3cc24e71
Viturbiko <vito.negrito@gmail.com>
2018-11-07 22:34:57 +0100

    V1.0

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,21 +1,48 @@
-<nav>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-    <?php function activeMenu($url)
-    {
-        return request()->is($url) ? 'active_section' : '';
-    }
-    ?>
+<header>
 
-    <div class="toggle_navbar">
-        <i class="fas fa-bars menu" aria hidden="true"></i>
-    </div>
-    <ul>
-        <a href="{{ route('index') }}"><img src="/images/med-atlantic-logo.svg" alt="Logo"></a>
-        <li class="{{ activeMenu('/') }}"><a href="{{ route('index') }}">HOME</a></li>
-        <li class="{{ activeMenu('about') }}"><a href="{{ route('about') }}">THE PROJECT</a></li>
-        <li><a href="../#partners">PARTNERS</a></li>
-        <li class="{{ activeMenu('downloads') }}"><a href="{{ route('downloads') }}">DOWNLOADS</a></li>
-        <li class="{{ activeMenu('calendar') }}"><a href="{{ route('calendar') }}">EVENTS</a></li>
-        <li><a href="#contact">CONTACT</a></li>
-    </ul>
-</nav>
+    <img class="hamburger" src="/images/hamburger.png" alt="MENU">
+
+    <a href="{{ route('index') }}"><img class="logo_img" src="/images/med-atlantic-logo.svg" alt="Logo"></a>
+    
+    <nav>
+
+        <?php function activeMenu($url)
+        {
+            return request()->is($url) ? 'active_section' : '';
+        }
+        ?>
+
+        <ul>
+            <img class="close_menu" src="/images/X.png" alt="CLOSE">
+            <li class="hide_please"><a style="width:100%;" href="{{ route('index') }}"><img class="logo_img_min" src="/images/med-atlantic-logo.svg" alt="Logo"></a></li>
+            <a href="{{ route('index') }}"><img class="logo_img_fake" src="/images/med-atlantic-logo.svg" alt="Logo"></a>
+            <li class="{{ activeMenu('/') }}"><a href="{{ route('index') }}">HOME</a></li>
+            <li class="{{ activeMenu('about') }}"><a href="{{ route('about') }}">THE PROJECT</a></li>
+            <li><a href="../#partners">PARTNERS</a></li>
+            <li class="{{ activeMenu('downloads') }}"><a href="{{ route('downloads') }}">DOWNLOADS</a></li>
+            <li class="{{ activeMenu('calendar') }}"><a href="{{ route('calendar') }}">EVENTS</a></li>
+            <li><a href="#contact">CONTACT</a></li>
+        </ul>
+
+    </nav>
+
+</header>
+
+
+<script>
+
+    $('.hamburger').click(function(){
+        $('nav').animate({
+            right: '0'
+        });
+    });
+
+    $('.close_menu').click(function(){
+        $('nav').animate({
+            right: '-100%'
+        });
+    });
+
+</script>
